94a512e66ec75e332daef7ac63693323a5b0eb15
ladace <ladacez@gmail.com>
2012-12-10 21:32:40 +0800

    simple version

--- Main.ml
+++ Main.ml
@@ -6,7 +6,18 @@ open Type
 open Printf
 open Int32
 
-type context = { local : scope; }
+type var = {
+    name : string;
+    is_clock : bool;
+    vtype : var_type;
+    value : tval list
+}
+and tval = Undefined | Val of value
+and context = { local : (string * var) list; input : (string * value list) list }
+
+let make_var (namelist, t, csexpr) = map (function varid -> 
+    {name = (fst varid); is_clock = (snd varid); vtype = t; value = [] }) namelist
+let make_var_list lst = concat (map make_var lst)
 
 let find_replace func lst = match fold_left (fun (found, res) elem ->
         if found
@@ -26,12 +37,12 @@ let format_var_type = function
   | TIdent v -> v
 
 
-let print_scope { vars = vlst } =
+let print_var_list vlst  =
     print_string "scope:\n";
     print_list (fun (name, var) -> print_string name) vlst;
     print_newline ()
 
-let print_context context = print_scope context.local
+let print_context context = print_var_list context.local
 
 let rec print_program program =
     printf "The program contains %d nodes:\n" (length program.nodes);
@@ -59,81 +70,42 @@ and print_params pms =
         print_string (format_var_type var_type);
         print_newline ()
 
-let lookup context name = assoc name context.local.vars
-and bind_var context (name:string) value : context =
-    let tbl = context.local.vars in
+let next_clock context =
+    try
+        let grab_input name = if mem_assoc name context.input
+        then Val (hd (assoc name context.input))
+        else Undefined in
+        { context with
+            local = map (fun (name, vr) -> (name, { vr with value = (grab_input vr.name) :: vr.value })) context.local;
+            input = map (fun (name, lst) -> (name, tl lst)) context.input; }
+    with
+        Failure t when t = "hd" || t = "tl" -> raise (Failure "reach the end of input")
+
+let lookup context name = assoc name context.local
+and bind_var context (name:string) (value:value) : context =
+    let tbl = context.local in
+    let vr = assoc name tbl in
     { context with local =
-        { context.local with vars = (name, {(assoc name tbl) with value = value}) :: (remove_assoc name tbl) } }
+        (name, {vr with value = (Val value) :: tl vr.value}) :: (remove_assoc name tbl) }
 
-
-let vmult a b =
-    match (a, b) with
-        (VInt x, VInt y) -> VInt (mul x y)
-      | (VReal x, VReal y) -> VReal (x *. y)
-      | _ -> raise (Failure "Can't multiply")
-
-let vadd a b =
-    match (a, b) with
-        (VInt x, VInt y) -> VInt (add x y)
-      | (VReal x, VReal y) -> VReal (x +. y)
-      | _ -> raise (Failure "Can't add")
-
-let vminus a b =
-    match (a, b) with
-        (VInt x, VInt y) -> VInt (sub x y)
-      | (VReal x, VReal y) -> VReal (x -. y)
-      | _ -> raise (Failure "Can't minus")
-
-let vdivide a b =
-    match (a, b) with
-        (VReal x, VReal y) -> VReal (x /. y)
-      | _ -> raise (Failure "Can't divide")
-
-let vdiv a b =
-    match (a, b) with
-        (VInt x, VInt y) -> VInt (div x y)
-      | _ -> raise (Failure "Can't div")
-
-let vmod a b =
-    match (a, b) with
-        (VInt x, VInt y) -> VInt (rem x y)
-      | _ -> raise (Failure "Can't mod")
-
-let vneg a =
-    match a with
-        VInt x -> VInt (neg x)
-      | VReal x -> VReal (-.x)
-      | _ -> raise (Failure "Can't negate")
-
-let vreal_conv a =
-    match a with
-        VInt x -> VReal (to_float x)
-      | VReal x -> VReal x
-      | _ -> raise (Failure "Can't convert to real")
-
-let vint_conv a =
-    match a with
-        VInt x -> VInt x
-      | VReal x -> VInt (of_float x)
-      | _ -> raise (Failure "Can't convert to int")
 
 let rec eval_expr context eqs expr =
     let get_val x =
         let check var : value =
-            if var.value == VUndefined then
-                snd (solve_var context eqs var.name)
-            else var.value
+            match hd var.value with
+              Undefined -> snd (solve_var context eqs var.name)
+            | Val v -> v
         in match x with
             VIdent varname -> check (lookup context varname)
           | t -> t
             in let eval = eval_expr context eqs
                 in match expr with
-                    Add (a, b)    -> vadd (eval a) (eval b)
-                  | Minus (a, b)  -> vminus (eval a) (eval b)
-                  | Mult (a, b)   -> vmult (eval a) (eval b)
+                    Add    (a, b) -> vadd (eval a) (eval b)
+                  | Minus  (a, b) -> vminus (eval a) (eval b)
+                  | Mult   (a, b) -> vmult (eval a) (eval b)
                   | Divide (a, b) -> vdivide (eval a) (eval b)
-                  | Div (a, b)    -> vdiv (eval a) (eval b)
-                  | Mod (a, b)    -> vmod (eval a) (eval b)
+                  | Div    (a, b) -> vdiv (eval a) (eval b)
+                  | Mod    (a, b) -> vmod (eval a) (eval b)
                   | Neg a         -> vneg (eval a)
                   | RealConv a    -> vreal_conv (eval a)
                   | IntConv a     -> vint_conv (eval a)
@@ -142,34 +114,42 @@ let rec eval_expr context eqs expr =
                   | Temp a        -> raise (Failure "Not supported")
 
 and solve_var context eqs varname : context * value =
-    let value = (lookup context varname).value in
-        if value == VUndefined then
-            let eq = find (fun (lhs, expr) -> exists
-            (function LIdent name -> name = varname | _ -> false) lhs) eqs in
-            let result = eval_expr context eqs (snd eq)
-            and bind_lhs context (vr,v) = match vr with
-                LIdent varname -> bind_var context varname v
-              | Underscore -> context
-            and lhs = fst eq
-            in ((match result with
-                VList lst -> fold_left bind_lhs context (combine lhs lst)
-              | t -> bind_lhs context (hd lhs, t)), result)
-        else (context, value)
+    let value = hd (lookup context varname).value in
+    if value == Undefined then
+        let eq = find
+                 (fun (lhs, expr) -> exists
+                    (function LIdent name -> name = varname | _ -> false) lhs) eqs in
+
+        let result = eval_expr context eqs (snd eq)
+
+        and bind_lhs context (vr,v) = match vr with
+          LIdent varname -> bind_var context varname v
+        | Underscore -> context
+
+        and lhs = fst eq in
+
+        ((match result with
+          VList lst -> fold_left bind_lhs context (combine lhs lst)
+        | t -> bind_lhs context (hd lhs, t)), result)
+    else match value with Val v -> (context, v)
 
 let run_node { header=(_, _, args, rets); equations=eqs } input =
     (* Build vars *)
-    let vars_table = map (fun v -> (v.name, v))
-    and bind_var_list vars vals = map (fun vr -> {vr with value = hd (assoc vr.name
-vals)}) vars in
-        let output_vars = make_var_list rets in
-        let context = { local = { vars = vars_table
-                                (append (bind_var_list (make_var_list args) input)
-                                (output_vars))} } in
-        let (context, output) = fold_left (fun (context, res) var -> let (c, r) = (solve_var context eqs var.name)
-                                                          in (c, r::res))
-                        (context, []) output_vars
-        in print_context context;
-           iter print_value output
+    let vars_table = map (fun v -> (v.name, v)) in
+    let output_vars = make_var_list rets in
+    let context = { local = vars_table
+                            (append (make_var_list args)
+                            (output_vars));
+                    input = input } in
+    let rec cycle context =
+    let (context, output) = fold_left (fun (context, res) var -> let (c, r) =
+        (solve_var context eqs var.name)
+                                                        in (c, r::res))
+                    (context, []) output_vars in
+
+    print_list print_value output;
+    print_newline ();
+    cycle (next_clock context) in cycle (next_clock context)
 
 
 let _ =
@@ -184,7 +164,4 @@ let _ =
     (Parse_Error str) ->
         printf "Error: %s\n" str;
         exit 0
-    | e ->
-        print_string "Error!\n";
-        raise e
 
--- Type.ml
+++ Type.ml
@@ -11,17 +11,18 @@ and lvalue = LIdent of string | Underscore
 and value = VBool of bool | VInt of int32 | VChar of char | VReal of float
           | VIdent of string
           | VList of value list
-          | VUndefined
-and expr = RValue of value | Elist of expr list | Temp of int
-           | IntConv  of expr
-           | RealConv of expr
-           | Neg     of expr
-           | Add     of expr * expr
-           | Minus   of expr * expr
-           | Mult    of expr * expr
-           | Divide  of expr * expr
-           | Div     of expr * expr
-           | Mod     of expr * expr
+          | VNil
+(* and stream = { run: value list -> stream * value } *)
+and expr = RValue   of value | Elist of expr list | Temp of int
+         | IntConv  of expr
+         | RealConv of expr
+         | Neg      of expr
+         | Add      of expr * expr
+         | Minus    of expr * expr
+         | Mult     of expr * expr
+         | Divide   of expr * expr
+         | Div      of expr * expr
+         | Mod      of expr * expr
 
 
 type node = {
@@ -34,23 +35,61 @@ type program = {
     types : int list
 }
 
-type var = {
-    name : string;
-    is_clock : bool;
-    vtype : var_type;
-    value : value
-}
-and scope = {
-    vars : (string * var) list;
-}
-and scope_type = SInput | SOutput | SLocal
+(* Calculation *)
+let vmult a b =
+    match (a, b) with
+        (VInt x, VInt y) -> VInt (mul x y)
+      | (VReal x, VReal y) -> VReal (x *. y)
+      | _ -> raise (Failure "Can't multiply")
+
+let vadd a b =
+    match (a, b) with
+        (VInt x, VInt y) -> VInt (add x y)
+      | (VReal x, VReal y) -> VReal (x +. y)
+      | _ -> raise (Failure "Can't add")
+
+let vminus a b =
+    match (a, b) with
+        (VInt x, VInt y) -> VInt (sub x y)
+      | (VReal x, VReal y) -> VReal (x -. y)
+      | _ -> raise (Failure "Can't minus")
+
+let vdivide a b =
+    match (a, b) with
+        (VReal x, VReal y) -> VReal (x /. y)
+      | _ -> raise (Failure "Can't divide")
+
+let vdiv a b =
+    match (a, b) with
+        (VInt x, VInt y) -> VInt (div x y)
+      | _ -> raise (Failure "Can't div")
+
+let vmod a b =
+    match (a, b) with
+        (VInt x, VInt y) -> VInt (rem x y)
+      | _ -> raise (Failure "Can't mod")
+
+let vneg a =
+    match a with
+        VInt x -> VInt (neg x)
+      | VReal x -> VReal (-.x)
+      | _ -> raise (Failure "Can't negate")
+
+let vreal_conv a =
+    match a with
+        VInt x -> VReal (to_float x)
+      | VReal x -> VReal x
+      | _ -> raise (Failure "Can't convert to real")
+
+let vint_conv a =
+    match a with
+        VInt x -> VInt x
+      | VReal x -> VInt (of_float x)
+      | _ -> raise (Failure "Can't convert to int")
+
 
 (* Helpers *)
 let node_name (_, name, _, _) = name (* receive header and return name*)
-
-let make_var (namelist, t, csexpr) = map (function varid -> 
-    {name = (fst varid); is_clock = (snd varid); vtype = t; value = VUndefined}) namelist
-let make_var_list lst = concat (map make_var lst)
 
 let rec print_list func lst = match lst with
     fst :: snd :: rest -> func fst; print_char ','; print_list func (snd::rest)
@@ -58,10 +97,10 @@ let rec print_list func lst = match lst with
   | [] -> ()
 
 let rec print_value value = match value with
-    VBool b -> printf "Bool: %b" b
-  | VInt i -> printf "Int: %d" (to_int i)
-  | VChar c -> printf "Char: %c" c
-  | VReal r -> printf "Real: %f" r
-  | VIdent v -> printf "Undefined: %s" v
-  | VList v -> print_string "VList: "; print_list print_value v
-  | VUndefined -> printf "Undefined"
+    VBool  b   -> printf "Bool: %b" b
+  | VInt   i   -> printf "Int: %d" (to_int i)
+  | VChar  c   -> printf "Char: %c" c
+  | VReal  r   -> printf "Real: %f" r
+  | VIdent v   -> printf "Undefined: %s" v
+  | VList  v   -> print_string "VList: "; print_list print_value v
+  | VNil       -> print_string "Nil"
